032e367457b834d056d0962c4d85037c12624fde
Ace-Aryal <153974691+Ace-Aryal@users.noreply.github.com>
2024-03-25 22:02:14 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["*"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      fontFamily: {
+        "Josefin Sans"  : [ 'Josefin Sans' , 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
